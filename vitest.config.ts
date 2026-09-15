7a488fb9ad416efe6c7745272184c373fb8ad674
ij23031 <ij23031@edu.lu.lv>
2026-09-15 23:35:45 +0300

    fix(ci): separate integration coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vitest/config';
+import { configDefaults, defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
@@ -7,6 +7,7 @@ export default defineConfig({
       provider: 'v8',
       reporter: ['text', 'json-summary'],
     },
+    exclude: [...configDefaults.exclude, '**/test-integration/**'],
     include: ['packages/**/*.test.ts'],
   },
 });
